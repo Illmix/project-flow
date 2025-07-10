3dc34ee4ddf6844663cfe4bfeb122f1894004ed7
illmix <illachaban1100@gmail.com>
2025-07-10 21:32:21 +0300

    server set up

---
+++ backend/server.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import { typeDefs } from './src/graphql/typeDefs';
+import { resolvers } from './src/graphql/resolvers';
+
+import { createContext } from './src/context';
+
+const PORT = parseInt(process.env.PORT || '4000', 10);
+
+async function startApolloServer() {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    const { url } = await startStandaloneServer(server, {
+        context: createContext,
+
+        listen: { port: PORT },
+    });
+
+    console.log(`Server started on url : ${url}`);
+}
+
+startApolloServer().then(r => console.log());
