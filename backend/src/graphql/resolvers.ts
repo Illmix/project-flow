1bbc7015ca0007c087877fe684081ff75f9d5f00
illmix <illachaban1100@gmail.com>
2025-07-11 18:50:35 +0300

    resolvers and typedefs update

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -1,5 +1,4 @@
-import { merge } from 'lodash';
 import { authResolvers } from '../features/auth/auth.resolvers';
 import { usersResolvers } from '../features/users/users.resolvers';
 
-export const resolvers = merge(authResolvers, usersResolvers);
+export const resolvers = [authResolvers, usersResolvers];
--- backend/src/graphql/typeDefs.ts
+++ backend/src/graphql/typeDefs.ts
@@ -1,1 +1,15 @@
-export const typeDefs = ``;
+import { readFileSync } from 'fs';
+import path from 'path';
+import { globSync } from 'glob';
+
+// Load the base schema file.
+const baseSchemaPath = path.join(__dirname, 'base.graphql');
+const baseTypeDefs = readFileSync(baseSchemaPath, { encoding: 'utf-8' });
+
+// Find all .graphql files within the 'features' directory.
+const featureSchemasPath = path.join(__dirname, '../features/**/*.graphql');
+const featureTypeDefs = globSync(featureSchemasPath).map((filePath) =>
+    readFileSync(filePath, { encoding: 'utf-8' })
+);
+
+export const typeDefs = [baseTypeDefs, ...featureTypeDefs];
