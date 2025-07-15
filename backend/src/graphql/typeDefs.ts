b76a50ef5597b56600ddcaf6042239d843a36318
illmix <illachaban1100@gmail.com>
2025-07-15 23:25:41 +0300

    load typeDefs with graphql-tools

--- backend/src/graphql/typeDefs.ts
+++ backend/src/graphql/typeDefs.ts
@@ -1,26 +1,14 @@
-import { readFileSync } from 'fs';
-import { globSync } from 'glob';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Load the base schema file.
-const baseTypeDefs = readFileSync(join(__dirname, 'base.graphql'), { encoding: 'utf-8' });
+const baseSchemaPath = loadFilesSync(join(__dirname, 'base.graphql'));
 
-const featuresDirectory = join(__dirname, '../features');
-
-// Find all .graphql files within the 'features' directory.
-const featureSchemaFilePaths = globSync('**/*.graphql', {
-    cwd: featuresDirectory,
-    absolute: true, // This ensures glob returns the full, absolute path to the files.
-});
-
-// Read the content of each found file.
-const featureTypeDefs = featureSchemaFilePaths.map((filePath) =>
-    readFileSync(filePath, { encoding: 'utf-8' })
-);
-
-
-export const typeDefs = [baseTypeDefs, ...featureTypeDefs];
+const typesArray = loadFilesSync(join(__dirname, '../features/**/*.graphql'));
+typesArray.push(baseSchemaPath);
+export const typeDefs = mergeTypeDefs(typesArray);
