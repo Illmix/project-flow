e3a6217ec9a1c5fe4121eab7e795cff6de420d57
illmix <illachaban1100@gmail.com>
2025-07-20 19:56:50 +0300

    DateTime scalar implement

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -1,8 +1,10 @@
+import {DateTimeResolver} from "graphql-scalars";
 import { authResolvers } from '../features/auth/auth.resolvers.js';
 import { usersResolvers } from '../features/users/users.resolvers.js';
 import {skillsResolvers} from "../features/skills/skills.resolvers.js";
 
 export const resolvers = [
+    { DateTime: DateTimeResolver },
     authResolvers,
     usersResolvers,
     skillsResolvers
