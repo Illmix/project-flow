7a2013e133bfcbc1bf746e5dd1c82114178a9c86
illmix <illachaban1100@gmail.com>
2025-07-20 16:45:11 +0300

    create getSkills and getSkill resolvers

--- backend/src/features/skills/skills.resolvers.ts
+++ backend/src/features/skills/skills.resolvers.ts
@@ -2,5 +2,20 @@ import type {Resolvers} from '../../graphql/types.js';
 import {authenticated} from "../../lib/permissions.js";
 
 export const skillsResolvers: Resolvers = {
-
+    Query: {
+        /**
+         * @description Fetches a list of all skills.
+         */
+        getSkills: authenticated(async (_parent, _args, context) => {
+            return context.prisma.skill.findMany();
+        }),
+        /**
+         * @description Fetches a single skill by ID.
+         */
+        getSkill: authenticated(async (_parent, { id }, context) => {
+            return context.prisma.skill.findUnique({
+                where: { id },
+            });
+        }),
+    }
 }
