5abc802bfed9751d17543d8dc10f86c3ac09e12b
illmix <illachaban1100@gmail.com>
2025-07-22 17:30:59 +0300

    create project resolvers and add them to the global resolvers

---
+++ backend/src/features/projects/project.resolvers.ts
@@ -0,0 +1,59 @@
+import type {Resolvers} from '../../graphql/types.js';
+import {authenticated, isProjectOwner} from "../../lib/permissions.js";
+import {randomUUID} from "crypto";
+
+
+export const projectsResolvers: Resolvers = {
+    Query: {
+        /**
+         * @description Fetches a list of all projects.
+         */
+        getProjects: authenticated(async (_parent, _args, context) => {
+            return context.prisma.project.findMany({
+                orderBy: {created_at: 'desc'},
+            });
+        }),
+        /**
+         * @description Fetches a single project by its public ID.
+         */
+        getProject: authenticated(async (_parent, { publicId }, context) => {
+            return context.prisma.project.findUnique({
+                where: { publicId },
+            });
+        }),
+    },
+    Mutation: {
+        /**
+         * @description Creates a new project.
+         */
+        createProject: authenticated(async (_parent, { input }, context) => {
+            const currentEmployee = context.currentEmployee;
+            if (!currentEmployee) throw new Error("Authentication error.");
+
+            return context.prisma.project.create({
+                data: {
+                    ...input,
+                    publicId: randomUUID().slice(0, 8),
+                    createdById: currentEmployee.id
+                },
+            });
+        }),
+        /**
+         * @description Updates an existing project.
+         */
+        updateProject: isProjectOwner(async (_parent, { publicId, input }, context) => {
+            return context.prisma.project.update({
+                where: {publicId},
+                data: {...input},
+            });
+        }),
+        /**
+         * @description Deletes a project.
+         */
+        deleteProject: isProjectOwner(async (_parent, { publicId }, context) => {
+            return context.prisma.project.delete({
+                where: {publicId},
+            });
+        }),
+    },
+}
