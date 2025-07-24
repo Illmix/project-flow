09a89e88f1cc980c15b36b78233ff1586507ee30
illmix <illachaban1100@gmail.com>
2025-07-24 21:55:46 +0300

    test: add 'delete task by publicId' mutation test

--- backend/src/features/tasks/task.test.ts
+++ backend/src/features/tasks/task.test.ts
@@ -68,62 +68,62 @@ describe('Task Resolvers', () => {
     })
 
 
-        it('should create a new task in the project', async () => {
-        const {context: contextValue, employee} = await createAuthenticatedContext(prisma);
+    it('should create a new task in the project', async () => {
+    const {context: contextValue, employee} = await createAuthenticatedContext(prisma);
 
-        const project = await contextValue.prisma.project.create({
-            data: {
-                Name: 'Test Project',
-                publicId: 'testproject-123',
-                createdById: employee.id,
-            },
-        });
+    const project = await contextValue.prisma.project.create({
+        data: {
+            Name: 'Test Project',
+            publicId: 'testproject-123',
+            createdById: employee.id,
+        },
+    });
 
-        const reactSkill = await prisma.skill.create({ data: { Name: 'React' } });
-        const nodeSkill = await prisma.skill.create({ data: { Name: 'Node.js' } });
+    const reactSkill = await prisma.skill.create({ data: { Name: 'React' } });
+    const nodeSkill = await prisma.skill.create({ data: { Name: 'Node.js' } });
 
-        const response = await server.executeOperation(
-            {
-                query: `
-                    mutation CreateTask($input: CreateTaskInput!) {
-                        createTask(input: $input) {
-                            publicId
+    const response = await server.executeOperation(
+        {
+            query: `
+                mutation CreateTask($input: CreateTaskInput!) {
+                    createTask(input: $input) {
+                        publicId
+                        Name
+                        Description
+                        Status
+                        project {
                             Name
-                            Description
-                            Status
-                            project {
-                                Name
-                            }
-                            requiredSkills {
-                                Name
-                            }
+                        }
+                        requiredSkills {
+                            Name
                         }
                     }
-                `,
-                variables: {
-                    input: {
-                        Name: 'New Test Task',
-                        Status: "new",
-                        Description: 'A task for testing.',
-                        projectPublicId: project.publicId,
-                        requiredSkillIds: [reactSkill.id, nodeSkill.id]
-                    },
+                }
+            `,
+            variables: {
+                input: {
+                    Name: 'New Test Task',
+                    Status: "new",
+                    Description: 'A task for testing.',
+                    projectPublicId: project.publicId,
+                    requiredSkillIds: [reactSkill.id, nodeSkill.id]
                 },
             },
-            { contextValue }
-        );
+        },
+        { contextValue }
+    );
 
-        if (response.body.kind !== 'single') {
-            fail('Expected single result');
-        }
+    if (response.body.kind !== 'single') {
+        fail('Expected single result');
+    }
 
-        const task = response.body.singleResult.data?.createTask as Task
+    const task = response.body.singleResult.data?.createTask as Task
 
-        expect(task.Name).toBe('New Test Task');
-        expect(task.Description).toBe('A task for testing.');
-        expect(task.project.Name).toBe('Test Project');
-        expect(task.requiredSkills).toEqual(
-            expect.arrayContaining([
+    expect(task.Name).toBe('New Test Task');
+    expect(task.Description).toBe('A task for testing.');
+    expect(task.project.Name).toBe('Test Project');
+    expect(task.requiredSkills).toEqual(
+        expect.arrayContaining([
                 expect.objectContaining({ Name: 'React' }),
                 expect.objectContaining({ Name: 'Node.js' }),
             ])
@@ -201,5 +201,54 @@ describe('Task Resolvers', () => {
                 expect.objectContaining({ Name: 'Python' }),
             ])
         );
+    })
+    it('should delete a task in the project', async () => {
+        const {context: contextValue, employee} = await createAuthenticatedContext(prisma);
+
+        const project = await contextValue.prisma.project.create({
+            data: {
+                Name: 'Test Project',
+                publicId: 'testproject-123',
+                createdById: employee.id,
+            },
+        });
+
+        const pythonSkill = await prisma.skill.create({ data: { Name: 'Python' } });
+
+        const task = await contextValue.prisma.task.create({
+            data: {
+                Name: 'Test task',
+                Status: 'new',
+                publicId: 'testtask-123',
+                project_id: project.id,
+                requiredSkills: {
+                    connect: [{id: pythonSkill.id}]
+                }
+            },
+        });
+
+
+        const response = await server.executeOperation(
+            {
+                query: `
+                    mutation DeleteTask($publicId: String!) {
+                        deleteTask(publicId: $publicId,) {
+                            publicId
+                        }
+                    }
+                `,
+                variables: {
+                    publicId: task.publicId,
+                },
+            },
+            { contextValue }
+        );
+
+        if (response.body.kind !== 'single') {
+            fail('Expected single result');
+        }
+
+        const deletedTask = response.body.singleResult.data?.deleteTask as Task
+        expect(deletedTask.publicId).toBe(task.publicId);
     })
 })
