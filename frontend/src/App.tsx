efe692a62115bcae2bc0772d7cb961bdbbcdd9c8
illmix <illachaban1100@gmail.com>
2025-07-31 15:10:38 +0300

    use routes in App.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
+import {Navigate, Route, Routes} from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 function App() {
 
   return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </>
+      <Routes>
+          {/* Public Route */}
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* --- Protected Routes --- */}
+          <Route element={<ProtectedRoute />}>
+
+              <Route path="/dashboard" element={<>{}</>} />
+              <Route path="/project/:publicId" element={<></>} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
   )
 }
 
