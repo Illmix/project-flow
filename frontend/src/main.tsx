d2482a3d71b8c19c56dc06b813a98718bc4fb3bf
illmix <illachaban1100@gmail.com>
2025-08-01 00:56:16 +0300

    use AuthProvider in main file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,15 @@ import App from './App.tsx'
 import {ApolloProvider} from "@apollo/client";
 import {client} from "./graphql/client.ts";
 import {BrowserRouter} from "react-router-dom";
+import {AuthProvider} from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ApolloProvider client={client}>
             <BrowserRouter>
-                <App />
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
             </BrowserRouter>
         </ApolloProvider>
     </StrictMode>,
