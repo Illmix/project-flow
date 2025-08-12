8d21fa04d3fa2d412372ca19fc97d7f40eaf4558
illmix <illachaban1100@gmail.com>
2025-08-12 14:08:35 +0300

    add icons to task columns

--- frontend/src/components/tasks/TaskBoard.tsx
+++ frontend/src/components/tasks/TaskBoard.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import TaskColumn from './TaskColumn';
 import {Task, TaskStatus} from "../../types/graphql.ts";
+import {Ban, CheckCircle2, Hourglass, Layers} from "lucide-react";
 
 type TaskBoardProps = {
     tasks: Task[];
@@ -23,10 +24,10 @@ const TaskBoard = ({ tasks }: TaskBoardProps) => {
 
     return (
         <div className="flex justify-between gap-10 overflow-x-auto p-2">
-            <TaskColumn title="Backlog" tasks={groupedTasks.new} />
-            <TaskColumn title="In Progress" tasks={groupedTasks.in_progress} />
-            <TaskColumn title="Done" tasks={groupedTasks.done} />
-            <TaskColumn title="Canceled" tasks={groupedTasks.canceled} />
+            <TaskColumn icon={<Layers className="mt-0.5" size={20}/>} title="Backlog" tasks={groupedTasks.new} />
+            <TaskColumn icon={<Hourglass className="mt-0.5" size={20}/>} title="In Progress" tasks={groupedTasks.in_progress} />
+            <TaskColumn icon={<CheckCircle2 className="mt-0.5" size={20}/>} title="Done" tasks={groupedTasks.done} />
+            <TaskColumn icon={<Ban className="mt-0.5" size={20}/>} title="Canceled" tasks={groupedTasks.canceled} />
         </div>
     );
 };
