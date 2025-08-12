9d771d00e6c0fc42ea249e3b499f1f18ec6fd35b
illmix <illachaban1100@gmail.com>
2025-08-12 13:56:51 +0300

    add truncate style if task name is too big

--- frontend/src/components/tasks/TaskCard.tsx
+++ frontend/src/components/tasks/TaskCard.tsx
@@ -7,7 +7,7 @@ type TaskCardProps = {
 const TaskCard = ({ task }: TaskCardProps) => {
     return (
         <div className="bg-slate-700/50 p-3 rounded-md border border-slate-700 shadow-sm">
-            <p className="text-sm text-slate-200">{task.Name}</p>
+            <p className="text-sm text-slate-200 truncate">{task.Name}</p>
         </div>
     );
 };
