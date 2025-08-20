67d8c598b8b2d375e557a46f80c1a682becf920c
illmix <illachaban1100@gmail.com>
2025-08-20 18:16:14 +0300

    make task card draggable item

--- frontend/src/components/tasks/TaskCard.tsx
+++ frontend/src/components/tasks/TaskCard.tsx
@@ -1,3 +1,5 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import { Task } from '../../types/graphql';
 import { Pencil, Trash2 } from 'lucide-react';
 import {MouseEvent} from "react";
@@ -9,6 +11,22 @@ type TaskCardProps = {
 };
 
 const TaskCard = ({ task, onEditClick, onDeleteClick }: TaskCardProps) => {
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+        isDragging, // Styles for the card while it's being dragged
+    } = useSortable({ id: task.publicId });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        opacity: isDragging ? 0.5 : 1,
+        boxShadow: isDragging ? '0px 5px 15px rgba(0, 0, 0, 0.2)' : 'none',
+    };
+
     const onTaskDeleteClick = (e: MouseEvent<HTMLButtonElement>, task: Task) => {
         e.stopPropagation();
         e.preventDefault();
@@ -16,10 +34,15 @@ const TaskCard = ({ task, onEditClick, onDeleteClick }: TaskCardProps) => {
     }
     return (
         <div
+            ref={setNodeRef}
+            style={style}
             onClick={() => onEditClick(task)}
+            {...attributes}
             className="bg-slate-700/50 p-3 cursor-pointer
             rounded-md border border-slate-700 shadow-sm group relative">
-            <p className="text-sm text-slate-200 truncate max-w-[85%]">{task.Name}</p>
+            <div {...listeners} className="cursor-grab touch-none">
+                <p className="text-sm text-slate-200 truncate max-w-[85%]">{task.Name}</p>
+            </div>
 
             <div className="absolute top-2 right-2 flex gap-1 opacity-0 group-hover:opacity-100 transition-opacity">
                 <button onClick={() => onEditClick(task)}
