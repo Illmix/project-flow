6a0a8b5864d7d532038ae9fa71b17672ad030ddf
illmix <illachaban1100@gmail.com>
2025-08-21 23:22:12 +0300

    fix updateTask mutation triggers when task moves in the same column

--- frontend/src/components/tasks/TaskColumn.tsx
+++ frontend/src/components/tasks/TaskColumn.tsx
@@ -2,7 +2,6 @@ import { Task } from '../../types/graphql';
 import TaskCard from './TaskCard';
 import {ReactNode} from "react";
 import { useDroppable } from '@dnd-kit/core';
-import { SortableContext } from '@dnd-kit/sortable';
 
 type TaskColumnProps = {
     id: string; // The ID is the status of the column
@@ -22,15 +21,13 @@ const TaskColumn = ({
                         id
 }: TaskColumnProps) => {
     const { setNodeRef } = useDroppable({ id });
-    const taskIds = tasks.map(task => task.publicId);
     return (
-        <div ref={setNodeRef} className="bg-slate-800/80 rounded-lg p-3 min-h-120 min-w-60 max-w-95 flex-1">
+        <div ref={setNodeRef} className="bg-slate-800/80 rounded-lg p-3 min-h-120 min-w-60 max-w-92 flex-1">
             <header className="border-b flex gap-2 justify-center border-slate-700">
                 <h3 className="text-md font-semibold text-slate-300 mb-3">{title}</h3>
                 {icon}
             </header>
             <div className="space-y-3 mt-3">
-                <SortableContext id={id} items={taskIds}>
                     {tasks.map(task => (
                         <TaskCard
                             key={task.publicId}
@@ -39,7 +36,6 @@ const TaskColumn = ({
                             onDeleteClick={onDeleteTask}
                         />
                     ))}
-                </SortableContext>
             </div>
         </div>
     );
