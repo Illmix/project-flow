d237af1544b60a9f980e2fa59ea4e85a2520bbb9
illmix <illachaban1100@gmail.com>
2025-09-02 16:06:38 +0300

    add deleteProject mutation to projectMutations hook

--- frontend/src/hooks/useProjectMutations.ts
+++ frontend/src/hooks/useProjectMutations.ts
@@ -1,11 +1,20 @@
 import { useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import { UPDATE_PROJECT_MUTATION } from '../graphql/mutations/projectMutations';
-import {UpdateProjectMutation, UpdateProjectMutationVariables} from '../types/graphql';
+import { GET_PROJECTS_QUERY } from '../graphql/queries/projectQueries';
+import { DELETE_PROJECT_MUTATION, UPDATE_PROJECT_MUTATION } from '../graphql/mutations/projectMutations';
+import {
+    DeleteProjectMutation, DeleteProjectMutationVariables,
+    GetProjectsQuery,
+    UpdateProjectMutation,
+    UpdateProjectMutationVariables
+} from '../types/graphql';
 
 export const useProjectMutations = () => {
+    const navigate = useNavigate();
 
-    const [updateProject, { loading: updateLoading }] = useMutation<UpdateProjectMutation, UpdateProjectMutationVariables>(
+    const [updateProject, { loading: updateLoading }] = useMutation<UpdateProjectMutation,
+        UpdateProjectMutationVariables>(
         UPDATE_PROJECT_MUTATION,
         {
             onCompleted: () => {
@@ -15,9 +24,33 @@ export const useProjectMutations = () => {
         }
     );
 
+    const [deleteProject, { loading: deleteLoading }] = useMutation<DeleteProjectMutation,
+        DeleteProjectMutationVariables>(
+        DELETE_PROJECT_MUTATION,
+        {
+            onCompleted: () => {
+                toast.success('Project deleted successfully!');
+                navigate('/projects');
+            },
+            onError: (err) => toast.error(`Error: ${err.message}`),
+            update(cache, { data: result }) {
+                const deletedProjectId = result?.deleteProject.publicId;
+                const existingProjects = cache.readQuery<GetProjectsQuery>({ query: GET_PROJECTS_QUERY });
+                if (deletedProjectId && existingProjects) {
+                    cache.writeQuery({
+                        query: GET_PROJECTS_QUERY,
+                        data: {
+                            getProjects: existingProjects.getProjects.filter(p => p.publicId !== deletedProjectId)
+                        },
+                    });
+                }
+            },
+        }
+    );
 
     return {
         updateProject,
-        loading: updateLoading,
+        deleteProject,
+        loading: updateLoading || deleteLoading,
     };
 };
